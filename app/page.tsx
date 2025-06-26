090b7c6dff9e8e672acd963a7b399ca1a4963eda
salmahadrikhoussa <salmahadri.k@gmail.com>
2025-06-26 11:17:54 +0100

    Redirection vers la page de login depuis /

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-5xl font-extrabold tracking-tight">
-        Welcome to{' '}
-        <a className="text-blue-600" href="https://nextjs.org">
-          Next.js!
-        </a>
-      </h1>
-    </main>
-  );
+  redirect("/login"); // Mets ici le chemin exact si différent
 }
